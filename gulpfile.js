8f746529747e353d963fbe30a5f46902a9a8892b
Julia Kharlamova <julia88.2010@mail.ru>
2018-12-06 22:09:28 +0300

    исправлен адаптив

--- gulpfile.js
+++ gulpfile.js
@@ -3,113 +3,26 @@ var bs = require('browser-sync');
 var sass = require('gulp-sass');
 var gutil = require( 'gulp-util' );
 var ftp = require( 'vinyl-ftp' );
-var concatCss = require('gulp-concat');
-var autoprefixer = require('gulp-autoprefixer');
-var htmlmin = require('gulp-htmlmin');
-var cleanCSS = require('gulp-clean-css');
-var fontmin = require('gulp-fontmin');
-var gulp = require('gulp');
-var imagemin = require('gulp-imagemin');
-var uglify = require('gulp-uglify');
-var pump = require('pump');
-var concatCss = require('gulp-concat-css');
-var concat = require('gulp-concat');
-var htmlhint = require('htmlhint');
-var rev = require('gulp-rev-append');
 
 
-
-gulp.task('concat', function() {
-  return gulp.src('src/styles/*.css')
-    .pipe(concat('style.css'))
-    .pipe(gulp.dest('src/styles'));
-});
-
-// Minify JS - Minifies JS
-gulp.task('uglify', function (cb) {
-  pump([
-        gulp.src('src/js/main.js'),
-        uglify(),
-        gulp.dest('dist/js')
-    ],
-    cb
-  );
-});
-
-
-gulp.task('minify', () => {
-  return gulp.src('src/*.html')
-    .pipe(htmlmin({ collapseWhitespace: true }))
-    .pipe(gulp.dest('dist'));
-});
-
-gulp.task('cleanCSS', () => {
-  return gulp.src('src/css/*.css')
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest('dist/css'));
-});
-
-gulp.task('imagemin', function () {
-    return gulp.src('src/img/dev/*')
-        .pipe(imagemin({
-            progressive: true,
-            svgoPlugins: [{removeViewBox: false}]
-        }))
-        .pipe(gulp.dest('dist/img'));
-});
-
-gulp.task('fontmin', function () {
-    return gulp.src('src/fonts/*.ttf')
-    	.pipe(gulp.src('src/fonts/*.eot'))
-    	.pipe(gulp.src('src/fonts/*.svg'))
-    	.pipe(gulp.src('src/fonts/*.woff'))
-    	.pipe(gulp.src('src/fonts/*.woff2'))
-        .pipe(fontmin({
-            text: '天地玄黄 宇宙洪荒',
-        }))
-        .pipe(gulp.dest('dist/fonts'));
-});
-
-
-
-// Static Server + watching scss/html files
+// Запускаем сервер, предварительно скопилировав SASS
 gulp.task('serve', ['sass'], function() {
 
     bs.init({
-        server: "./src"
+      server: "./src"
     });
 
     gulp.watch("src/sass/*.sass", ['sass']);
     gulp.watch("src/*.html").on('change', bs.reload);
 });
 
-// Compile sass into CSS & auto-inject into browsers
+// Делаем компиляцию SASS в CSS 
 gulp.task('sass', function() {
-    return gulp.src("src/sass/*.sass")
-        .pipe(sass())
-        .pipe(autoprefixer({
-            browsers: ['last 15 versions'],
-            cascade: false
-        }))
-        .pipe(concatCss("main.css"))
-        .pipe(gulp.dest("src/css"))
-        .pipe(bs.stream());
+ return gulp.src("src/sass/*.sass")
+   .pipe(sass())
+   .pipe(gulp.dest("src/css"))
+   .pipe(bs.stream());
 });
-
-// Htmlhint - Validate HTML
-gulp.task('htmlhint', function() {
-    gulp.src('src/*.html')
-        .pipe(htmlhint())
-        .pipe(htmlhint.reporter())
-        .pipe(htmlhint.failReporter({ suppress: true }))
-});
-
-/*очищает кэш
-gulp.task('rev', function() {
-  gulp.src('src/index.html')
-    .pipe(rev())
-    .pipe(gulp.dest('.'));
-});*/
 
 gulp.task('default', ['serve']);
 
@@ -123,7 +36,7 @@ gulp.task( 'deploy', function () {
 	} );
 
 	var globs = [
-		'./src/**',
+		'src/**'
 	];
 
 	// using base = '.' will transfer everything to /public_html correctly
@@ -133,4 +46,4 @@ gulp.task( 'deploy', function () {
 		.pipe( conn.newer( '/www/kharlamova-23.ru' ) ) // only upload newer files
 		.pipe( conn.dest( '/www/kharlamova-23.ru' ) );
 
-} );
+});
